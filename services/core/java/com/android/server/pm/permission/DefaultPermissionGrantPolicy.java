76fb0340991e5950a7d5024d276b95bc43f3663f
DennySPB <dennyspb@gmail.com>
2019-08-26 16:03:02 +0300

    Fix Google Markup FC
    Grant runtime storage permissions.
    This fixes FC of google markup when using it along with Longshot
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I326a0ce9c0b7761552e7604a67bf4470045f6ab0

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -964,6 +964,11 @@ public final class DefaultPermissionGrantPolicy {
             grantRuntimePermissions(chromiumPackage, CONTACTS_PERMISSIONS, userId);
             grantRuntimePermissions(chromiumPackage, STORAGE_PERMISSIONS, userId);
         }
+        // Google Markup
+        PackageParser.Package googlemarkupPackage = getSystemPackage("com.google.android.markup");
+        if (googlemarkupPackage != null && doesPackageSupportRuntimePermissions(googlemarkupPackage)) {
+            grantRuntimePermissions(googlemarkupPackage, STORAGE_PERMISSIONS, userId);
+        }
         // Pixel Wallpaper
         PackageParser.Package pixelwallsPackage = getDefaultProviderAuthorityPackage("com.google.android.apps.wallpaper", userId);
         if (pixelwallsPackage != null) {
