syberia-project/platform_frameworks_base

GamingMode: allow disable navigation gestures [1/2]
Change-Id: I84dfb11fdb6d27d145511580943142084d3b2cdd
Signed-off-by: DennySPB <dennyspb@gmail.com>

core/java/com/android/internal/util/syberia/GamingModeController.java
@@ -73,6 +73,8 @@ public class GamingModeController {
     private static int mRingerState;
     private static int mZenState;
     private static int mHwKeysState;
+    private static int mEdgeGesturesState;
+    private static int mOpGesturesState;
     private static int mAdaptiveBrightness;
 
     public static final String GAMING_MODE_TURN_OFF = "android.intent.action.GAMING_MODE_TURN_OFF";
@@ -259,6 +261,21 @@ public class GamingModeController {
                 Settings.Secure.HARDWARE_KEYS_DISABLE, 1);
         }
 
+        // Gestures
+        boolean disableGestures = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.GAMING_MODE_GESTURES_TOGGLE, 0) == 1;
+        if (disableGestures) {
+            mEdgeGesturesState = Settings.Secure.getInt(mContext.getContentResolver(),
+                              Settings.Secure.EDGE_GESTURES_ENABLED, 0);
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.EDGE_GESTURES_ENABLED, 0);
+
+            mOpGesturesState = Settings.System.getInt(mContext.getContentResolver(),
+                              Settings.System.OMNI_USE_BOTTOM_GESTURE_NAVIGATION, 0);
+            Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.OMNI_USE_BOTTOM_GESTURE_NAVIGATION, 0);
+        }
+
         // Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
         int ringerMode = Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.GAMING_MODE_RINGER_MODE, 0);
@@ -301,6 +318,17 @@ public class GamingModeController {
             Settings.Secure.putInt(mContext.getContentResolver(),
                 Settings.Secure.HARDWARE_KEYS_DISABLE, mHwKeysState);
         }
+
+        // Gestures
+        boolean disableGestures = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.GAMING_MODE_GESTURES_TOGGLE, 0) == 1;
+        if (disableGestures) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.EDGE_GESTURES_ENABLED, mEdgeGesturesState);
+            Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.OMNI_USE_BOTTOM_GESTURE_NAVIGATION, mOpGesturesState);
+        }
+
         int ringerMode = Settings.System.getInt(mContext.getContentResolver(),
                  Settings.System.GAMING_MODE_RINGER_MODE, 0);
         if (ringerMode != 0 && (mRingerState != getRingerModeInternal() ||

Exhigh/android_frameworks_base

core/java/com/android/internal/util/rr/GamingModeController.java
@@ -73,6 +73,8 @@ public class GamingModeController {
     private static int mRingerState;
     private static int mZenState;
     private static int mHwKeysState;
+    private static int mEdgeGesturesState;
+    private static int mOpGesturesState;
     private static int mAdaptiveBrightness;
 
     public static final String GAMING_MODE_TURN_OFF = "android.intent.action.GAMING_MODE_TURN_OFF";
@@ -259,6 +261,21 @@ public class GamingModeController {
                 Settings.Secure.HARDWARE_KEYS_DISABLE, 1);
         }
 
+        // Gestures
+        boolean disableGestures = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.GAMING_MODE_GESTURES_TOGGLE, 0) == 1;
+        if (disableGestures) {
+            mEdgeGesturesState = Settings.Secure.getInt(mContext.getContentResolver(),
+                              Settings.Secure.EDGE_GESTURES_ENABLED, 0);
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.EDGE_GESTURES_ENABLED, 0);
+
+            mOpGesturesState = Settings.System.getInt(mContext.getContentResolver(),
+                              Settings.System.USE_BOTTOM_GESTURE_NAVIGATION, 0);
+            Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.USE_BOTTOM_GESTURE_NAVIGATION, 0);
+        }
+
         // Ringer mode (0: Off, 1: Vibrate, 2:DND: 3:Silent)
         int ringerMode = Settings.System.getInt(mContext.getContentResolver(),
             Settings.System.GAMING_MODE_RINGER_MODE, 0);
@@ -301,6 +318,17 @@ public class GamingModeController {
             Settings.Secure.putInt(mContext.getContentResolver(),
                 Settings.Secure.HARDWARE_KEYS_DISABLE, mHwKeysState);
         }
+
+        // Gestures
+        boolean disableGestures = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.GAMING_MODE_GESTURES_TOGGLE, 0) == 1;
+        if (disableGestures) {
+            Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.EDGE_GESTURES_ENABLED, mEdgeGesturesState);
+            Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.USE_BOTTOM_GESTURE_NAVIGATION, mOpGesturesState);
+        }
+
         int ringerMode = Settings.System.getInt(mContext.getContentResolver(),
                  Settings.System.GAMING_MODE_RINGER_MODE, 0);
         if (ringerMode != 0 && (mRingerState != getRingerModeInternal() ||
@@ -343,9 +371,9 @@ public class GamingModeController {
     }
 
     private void activateGamingMode(boolean enabled) {
-        if (mGamingModeActivated == enabled)
+        if (mGamingModeActivated == enabled && mGamingModeEnabled)
             return;
-        mGamingModeActivated = enabled;
+        mGamingModeActivated = enabled && mGamingModeEnabled;
         if (mGamingModeActivated) {
             enableGamingFeatures();
         } else {
@@ -391,8 +419,9 @@ public class GamingModeController {
                                    Settings.System.GAMING_MODE_ENABLED))) {
                 mGamingModeEnabled = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.GAMING_MODE_ENABLED, 0) == 1;
-                if (!mGamingModeEnabled) Settings.System.putInt(mContext.getContentResolver(),
-                    Settings.System.GAMING_MODE_ACTIVE, 0);
+                if (!mGamingModeEnabled)
+                    Settings.System.putInt(mContext.getContentResolver(),
+                         Settings.System.GAMING_MODE_ACTIVE, 0);
             } else if (uri.equals(Settings.System.getUriFor(
                                    Settings.System.GAMING_MODE_ACTIVE))) {
                 boolean enable = Settings.System.getInt(mContext.getContentResolver(),